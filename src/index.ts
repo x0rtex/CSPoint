386c7e84362fe353505bfbae6dff2665cda845eb
Alekss Belavskis <s00253368@atu.ie>
2025-10-16 16:45:59 +0100

    Added morgan for http logging

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import express, { Application, Request, Response } from "express";
+import morgan from "morgan";
 
 const PORT = process.env.PORT || 3000;
 
 const app: Application = express();
+
+app.use(morgan("dev"));
 
 app.get("/ping", async (_req: Request, res: Response) => {
   res.json({
